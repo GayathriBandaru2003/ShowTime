76145bfa1407505bc53eae63bb1de325d81e3c00
Anurag-verma18 <anuragkrverma18@gmail.com>
2024-02-09 12:32:44 +0530

    refactor: add pretty formatter to code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,76 +1,89 @@
-import {useRef, useState} from "react";
+import { useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidData } from "../utils/validate";
 
 const Login = () => {
-    const [isSignInForm, setIsSignInForm] = useState(true);
-    const [errorMessage, setErrorMessage] = useState(null);
-    const email = useRef(null);
-    const password = useRef(null);
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
 
-    const toggleSignInForm = () => {
-        setIsSignInForm(!isSignInForm);
-    };
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
 
-    const handleButtonClick = () => {
-        //validation
-        const message = checkValidData(email.current.value, password.current.value);
-        setErrorMessage(message);
-    };
+  const handleButtonClick = () => {
+    //validation
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+  };
 
-    return (
-        <div>
-            <Header />
-            <div className="absolute">
-                <img 
-                  src="https://assets.nflxext.com/ffe/siteui/vlv3/4da5d2b1-1b22-498d-90c0-4d86701dffcc/98a1cb1e-5a1d-4b98-a46f-995272b632dd/IN-en-20240129-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
-                  alt="bodyImage"
-                />
-            </div>
-            <form onSubmit={(e) => e.preventDefault() } 
-                  className="absolute w-1/3 p-12 my-24 mx-auto right-0 left-0 
-                bg-black text-white bg-opacity-80 rounded">
-                <h1 className="font-bold text-3xl py-4">
-                    {isSignInForm ? "Sign In" : "Sign Up"}
-                </h1>
-                {!isSignInForm && (
-                <input 
-                    className="p-4 my-4 w-full rounded bg-black bg-opacity-50
-                    border border-slate-400" 
-                    type="text" placeholder="Full Name" 
-                 />
-                )}
-                <input 
-                    className="p-4 my-4 w-full rounded bg-black bg-opacity-50
-                    border border-slate-400" 
-                    ref={email} type="text" placeholder="Email Address" 
-                />
-                <input 
-                    className="p-4 my-4 w-full rounded bg-black bg-opacity-50
-                    border border-slate-400" 
-                    ref={password} type="password" placeholder="Password" 
-                />
-                <p className="font-bold text-red-500 py-2 text-lg">{errorMessage}</p>
-                <button className="p-2 my-4 w-full rounded bg-red-700"
-                        onClick={handleButtonClick}
-                >
-                    {isSignInForm ? "Sign In" : "Sign Up"}
-                </button>
-                <p className="py-4 text-slate-400" onClick={toggleSignInForm}> 
-                    {isSignInForm ? "New to Netflix? " : "Already Registered? "}
-                    {isSignInForm 
-                      ? (<span 
-                           className="font-bold text-white cursor-pointer 
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/4da5d2b1-1b22-498d-90c0-4d86701dffcc/98a1cb1e-5a1d-4b98-a46f-995272b632dd/IN-en-20240129-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          alt="bodyImage"
+        />
+      </div>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute w-1/3 p-12 my-24 mx-auto right-0 left-0 
+                bg-black text-white bg-opacity-80 rounded"
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            className="p-4 my-4 w-full rounded bg-black bg-opacity-50
+                    border border-slate-400"
+            type="text"
+            placeholder="Full Name"
+          />
+        )}
+        <input
+          className="p-4 my-4 w-full rounded bg-black bg-opacity-50
+                    border border-slate-400"
+          ref={email}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          className="p-4 my-4 w-full rounded bg-black bg-opacity-50
+                    border border-slate-400"
+          ref={password}
+          type="password"
+          placeholder="Password"
+        />
+        <p className="font-bold text-red-500 py-2 text-lg">{errorMessage}</p>
+        <button
+          className="p-2 my-4 w-full rounded bg-red-700"
+          onClick={handleButtonClick}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 text-slate-400" onClick={toggleSignInForm}>
+          {isSignInForm ? "New to Netflix? " : "Already Registered? "}
+          {isSignInForm ? (
+            <span
+              className="font-bold text-white cursor-pointer 
                            hover:underline"
-                        >Sign Up Now.</span>)
-                      : (<span 
-                           className="font-bold text-white cursor-pointer 
+            >
+              Sign Up Now.
+            </span>
+          ) : (
+            <span
+              className="font-bold text-white cursor-pointer 
                            hover:underline"
-                        >Sign In Now.</span>)
-                    }
-                </p>
-            </form>
-        </div>
-    );
+            >
+              Sign In Now.
+            </span>
+          )}
+        </p>
+      </form>
+    </div>
+  );
 };
 export default Login;

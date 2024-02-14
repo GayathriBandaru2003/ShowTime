42124a59643b728ba316210932f95823448d1d18
Anurag-verma18 <anuragkrverma18@gmail.com>
2024-02-14 13:51:56 +0530

    feat: add movie api options in constants file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,11 @@
 export const APP_LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
 export const USER_AVATAR = "https://occ-0-6245-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
+
+export const MOVIE_API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjQ2NGNlMjZjMGU3YTE0ZjdlZjk2MzU5ZmNjMTdmYiIsInN1YiI6IjY1Y2M0ZTNkNGEwYjE5MDE2M2NlMTYzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a1l87QcCXSAL_fTeCiXpDuWC60VXqXQaZmAwZZFQR-E'
+    }
+  };

bab22b2b36accd7742d0a1b10b8de601d8acf2c6
Anurag-verma18 <anuragkrverma18@gmail.com>
2024-02-14 15:03:25 +0530

    feat: make API call to fetch movies list

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import { MOVIE_API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
 
 
 const Browse = () => {
+
+    const getNowPlayingMovies = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', MOVIE_API_OPTIONS);
+        const json = await data.json();
+        console.log(json);
+    }
+
+    useEffect(() => {
+        getNowPlayingMovies();
+    }, []);
+
     return  (
         <div>
             <Header />

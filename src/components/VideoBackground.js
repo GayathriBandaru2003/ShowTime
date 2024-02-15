2822a98b321b0d741c484cf3b2813ab388dee0df
Anurag-verma18 <anuragkrverma18@gmail.com>
2024-02-15 13:46:43 +0530

    add trailer video info to movieSlice and fetch key from there

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 import { MOVIE_API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
 
 
 const VideoBackground = ({movieId}) => {
+    const dispatch = useDispatch();
+    const trailerVideo = useSelector((store) => store?.movies?.trailerVideo);
 
     const getMovieVideos = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/955916/videos?language=en-US', MOVIE_API_OPTIONS);
@@ -11,6 +15,7 @@ const VideoBackground = ({movieId}) => {
         const filterData = json.results.filter((video) => video.name === "Official Trailer");
         const trailer = filterData.length ? filterData[0] : json.results[0];
         console.log(trailer);
+        dispatch(addTrailerVideo(trailer));
     };
     
     useEffect(() => {
@@ -19,7 +24,13 @@ const VideoBackground = ({movieId}) => {
 
     return (
         <div>
-            VideoBackground
+            <iframe
+            width="560" 
+            height="315" 
+            src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+            title="YouTube video player" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            ></iframe>
         </div>
     );
 };

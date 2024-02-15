079bcd1bc7651e6e0e71a13f1c3fa0929e48ccca
Anurag-verma18 <anuragkrverma18@gmail.com>
2024-02-15 14:04:13 +0530

    create custom hook to fetch trailer video info

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,26 +1,11 @@
-import { useEffect } from "react";
-import { MOVIE_API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../utils/moviesSlice";
-
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({movieId}) => {
-    const dispatch = useDispatch();
+
     const trailerVideo = useSelector((store) => store?.movies?.trailerVideo);
 
-    const getMovieVideos = async () => {
-        const data = await fetch('https://api.themoviedb.org/3/movie/955916/videos?language=en-US', MOVIE_API_OPTIONS);
-        const json = await data.json();
-        console.log(json);
-        const filterData = json.results.filter((video) => video.name === "Official Trailer");
-        const trailer = filterData.length ? filterData[0] : json.results[0];
-        console.log(trailer);
-        dispatch(addTrailerVideo(trailer));
-    };
-    
-    useEffect(() => {
-        getMovieVideos();
-    }, []);
+    useMovieTrailer(movieId);
 
     return (
         <div>

a1ea2af29ef7f868e2afc2332f7792fccf28ccb6
Anurag-verma18 <anuragkrverma18@gmail.com>
2024-02-15 10:41:24 +0530

    dispatch action for now playing movies and rename movie slice

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,14 +1,19 @@
 import { useEffect } from "react";
 import { MOVIE_API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 
 const Browse = () => {
 
+    const dispatch = useDispatch();
+
     const getNowPlayingMovies = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', MOVIE_API_OPTIONS);
         const json = await data.json();
-        console.log(json);
+        console.log(json.results);
+        dispatch(addNowPlayingMovies(json.results));
     }
 
     useEffect(() => {

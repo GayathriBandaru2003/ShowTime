383968e1ef101ce9e96ef417dd7daf048f33f287
Anurag-verma18 <anuragkrverma18@gmail.com>
2024-02-15 13:25:15 +0530

    fetch trailer video details from videos API

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
+import { MOVIE_API_OPTIONS } from "../utils/constants";
 
 
-const VideoBackground = () => {
+const VideoBackground = ({movieId}) => {
+
+    const getMovieVideos = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/955916/videos?language=en-US', MOVIE_API_OPTIONS);
+        const json = await data.json();
+        console.log(json);
+        const filterData = json.results.filter((video) => video.name === "Official Trailer");
+        const trailer = filterData.length ? filterData[0] : json.results[0];
+        console.log(trailer);
+    };
+    
+    useEffect(() => {
+        getMovieVideos();
+    }, []);
+
     return (
         <div>
             VideoBackground
